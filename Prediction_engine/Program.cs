5a33f2f1a09d3b65880c2d728bc1befec1b1ae16
Gabriele Vianello <vianello.tech@gmail.com>
2024-02-25 11:43:01 +0100

    model and dataset added

--- Prediction_engine/Program.cs
+++ Prediction_engine/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Prediction_engine;
+
+
+Console.WriteLine("The following program classifies \nthe comments of differents restaurants\n");
+int x = 1;
+while (x >= 0) {
+    //Load sample data
+    var sampleData = new MLModelSentID.ModelInput()
+    {
+        Col0 = Console.ReadLine(),
+    };
+
+    //Load model and predict output
+    var result = MLModelSentID.Predict(sampleData);
+    if (result.PredictedLabel > 0) Console.WriteLine("Thats good\n");
+    else Console.WriteLine("disgusting place\n");
+    x--;
+    Console.WriteLine("press enter to continue\n");
+    Console.ReadLine();
+}
